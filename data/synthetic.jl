c48e0fabbdbd0dd633d2079bff0c63457b9f11d9
Alfonso Landeros <alanderos@ucla.edu>
2021-06-23 03:10:29 -0700

    Finish restructuring data folder

--- data/synthetic.jl
+++
@@ -1,8 +0,0 @@
-using Random, DataFrames, FileIO
-Random.seed!(5357)
-(m, n) = (10^3, 500)
-y = randn(m);
-@. y = sign(y);
-X = randn(m, n) .+ y; # this should give two 'nicely' separated Guassian clusters
-label = [yi < 0 ? :A : :B for yi in y]
-DataFrame([X label], :auto) |> FileIO.save("data/synthetic.csv")
