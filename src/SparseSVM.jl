eca372bf915cdaa4c3bfda768310a57d46f8f716
Alfonso Landeros <alanderos@ucla.edu>
2021-06-29 22:14:00 -0700

    Add Nesterov acceleration to SD and fix prefactor on distance penalty.

--- src/SparseSVM.jl
+++ src/SparseSVM.jl
@@ -80,12 +80,12 @@ end
 ##### OBJECTIVE #####
 function eval_objective(Ab::AbstractVector, y, b, p, rho, k, intercept)
   T = eltype(Ab)
-  m = length(y)
+  m, n = length(y), length(b)
   obj = zero(T)
   for i in eachindex(b) # rho * |P(b) - b|^2 contribution; can reduce to BLAS.axpby!
     obj = obj + (p[i] - b[i])^2
   end
-  obj = rho * inv(k + intercept) * obj
+  obj = rho * inv(n -k + 1) * obj
   for i in eachindex(y) # |z - A*b|^2 contribution
     obj = obj + max(one(T) - y[i] * Ab[i], zero(T))^2 / m
   end
@@ -237,6 +237,7 @@ function annealing!(f, b, A, y, tol, k, intercept;
   rho = rho_init
   T = eltype(A) # should be more careful here to make sure BLAS works correctly
   (obj, old, iters) = (zero(T), zero(T), 0)
+  m, n = size(A)
 
   # check if svd(A) is needed
   if f isa typeof(sparse_direct!)
@@ -270,7 +271,7 @@ function annealing!(f, b, A, y, tol, k, intercept;
     p = copy(b)
     pvec, idx = get_model_coefficients(p, intercept)
     project_sparsity_set!(pvec, idx, k)
-    dist = norm(p - b) / sqrt(k + intercept)
+    dist = norm(p - b) / sqrt(n - k + 1)
     copyto!(b, p)
   else
     @views p = copy(b[:,1])
@@ -280,7 +281,7 @@ function annealing!(f, b, A, y, tol, k, intercept;
       @views b_j = b[:,j]
       copyto!(p, b_j)
       project_sparsity_set!(pvec, idx, k)
-      dist = dist + norm(p - b_j) / sqrt(k + intercept)
+      dist = dist + norm(p - b_j) / sqrt(n - k + 1)
       copyto!(b_j, p)
     end
   end
@@ -314,55 +315,71 @@ Solve the distance-penalized SVM with fixed `rho` via steepest descent.
 This version updates the coefficient vector `b` and can be used with `annealing`.
 """
 function sparse_steepest!(b::Vector{T}, A::Matrix{T}, y::Vector{T}, rho::T, tol::T, k::Int, intercept::Bool, extras; ninner::Int=1000, verbose::Bool=false) where T <: AbstractFloat
-#
-  (m, n) = size(A)
-  (obj, old, iters) = (zero(T), zero(T), 0)
-  (grad, increment) = (zeros(T, n), zeros(T, n))
-  p = copy(b) # for projection
-  (pvec, idx) = get_model_coefficients(p, intercept)
-  z = zeros(T, m)
-  Ab = A * b
-  project_sparsity_set!(pvec, idx, k)
-  old = eval_objective(Ab, y, b, p, rho, k, intercept)
-  for iter = 1:ninner
-    iters = iters + 1
-    @. grad = rho / (k+intercept) * (b - p) # penalty contribution
-    for i = 1:m
-      z[i] = - y[i] * max(one(T) - y[i] * Ab[i], zero(T))
+    #
+    (m, n) = size(A)
+    (obj, old, iters) = (zero(T), zero(T), 0)
+    (grad, increment) = (zeros(T, n), zeros(T, n))
+    p = copy(b) # for projection
+    (pvec, idx) = get_model_coefficients(p, intercept)
+    b_old = zeros(size(A, 2))
+    z = zeros(T, m)
+    Ab = A * b
+    project_sparsity_set!(pvec, idx, k)
+    old = eval_objective(Ab, y, b, p, rho, k, intercept)
+    
+    # initialize worker array for Nesterov acceleration
+    copyto!(b_old, b)
+    
+    for iter = 1:ninner
+        iters = iters + 1
+        @. grad = rho / (n-k+1) * (b - p) # penalty contribution
+        for i = 1:m
+            z[i] = - y[i] * max(one(T) - y[i] * Ab[i], zero(T))
+        end
+        BLAS.gemv!('T', 1/m, A, z, 1.0, grad)
+        s = norm(grad)^2 # optimal step size
+        mul!(Ab, A, grad)
+        t = 1/m * norm(Ab)^2
+        s = s / (t + rho/(n-k+1) * norm(grad)^2 + eps()) # add small bias to prevent NaN
+        @. increment = - s * grad
+        # counter = 0
+        for step = 0:3 # step halving
+            @. b = b + increment
+            copyto!(p, b)
+            project_sparsity_set!(pvec, idx, k)
+            mul!(Ab, A, b)
+            obj = eval_objective(Ab, y, b, p, rho, k, intercept)
+            if obj < old
+                break
+            else
+                @. b = b - increment
+                @. increment = 0.5 * increment
+                # counter += 1
+            end
+        end
+        
+        has_converged = abs(old - obj) < tol * (old + one(T)) # norm(grad) < tol
+        
+        if  has_converged
+            break
+        else
+            # Update objective.
+            old = obj
+            
+            # Nesterov acceleration.
+            γ = (iter - 1) / (iter + 2 - 1)
+            @inbounds for i in eachindex(b)
+                xi = b[i]
+                yi = b_old[i]
+                zi = xi + γ * (xi - yi)
+                b_old[i] = xi
+                b[i] = zi
+            end
+        end
+        
     end
-    BLAS.gemv!('T', 1/m, A, z, 1.0, grad)
-    s = norm(grad)^2 # optimal step size
-    mul!(Ab, A, grad)
-    t = 1/m * norm(Ab)^2
-    s = s / (t + rho/(k+intercept) * norm(grad)^2 + eps()) # add small bias to prevent NaN
-    @. increment = - s * grad
-    # counter = 0
-    for step = 0:3 # step halving
-      @. b = b + increment
-      copyto!(p, b)
-      project_sparsity_set!(pvec, idx, k)
-      mul!(Ab, A, b)
-      obj = eval_objective(Ab, y, b, p, rho, k, intercept)
-      if obj < old
-        break
-      else
-        @. b = b - increment
-        @. increment = 0.5 * increment
-        # counter += 1
-      end
-    end
-    # println(counter, " step halving iterations")
-#     if iter <= 10 || mod(iter, 10) == 0 
-#       println("iter = ",iter," obj = ",obj)
-#     end
-    if  abs(old - obj) < tol * (old + one(T)) # norm(grad) < tol
-      break
-    else
-      old = obj
-    end  
-  end
-  verbose && print(iters,"  ",obj)
-  return b, iters, obj
+    verbose && print(iters,"  ",obj)
+    return b, iters, obj
 end
 
 """
@@ -382,60 +399,62 @@ Solve the distance-penalized SVM with fixed `rho` via normal equations.
 This version updates the coefficient vector `b` and can be used with `annealing`.
 """
 function sparse_direct!(b::Vector{T}, A::Matrix{T}, y::Vector{T}, rho::T, tol::T, k::Int, intercept::Bool, extras; ninner::Int=1000, verbose::Bool=false) where T <: AbstractFloat
-  (obj, old, iters) = (zero(T), zero(T), 0)
-  p = copy(b) # for projection
-  (pvec, idx) = get_model_coefficients(p, intercept)
-
-  # unpack
-  U, s, V = extras.U, extras.s, extras.V
-  z = extras.z
-  b_old = extras.b_old
-  
-  # initialize
-  Ab = A * b
-  
-  # initialize projection
-  project_sparsity_set!(pvec, idx, k)
-  
-  # initialize objective
-  old = eval_objective(Ab, y, b, p, rho, k, intercept)
-  
-  # initialize worker array for Nesterov acceleration
-  copyto!(b_old, b)
-  
-  for iter = 1:ninner
-      iters = iters + 1
-      
-      # Update estimates
-      compute_z!(z, Ab, y)
-      update_beta!(b, U, s, V, z, p, rho / (k + intercept))
-      
-      # Nesterov acceleration
-      γ = (iter - 1) / (iter + 2 - 1)
-      @inbounds for i in eachindex(b)
-          xi = b[i]
-          yi = b_old[i]
-          zi = xi + γ * (xi - yi)
-          b_old[i] = xi
-          b[i] = zi
-      end
-      
-      # update objective
-      copyto!(p, b)
-      project_sparsity_set!(pvec, idx, k)
-      mul!(Ab, A, b)
-      obj = eval_objective(Ab, y, b, p, rho, k, intercept)
-      # if iter <= 10 || mod(iter, 10) == 0 
-      #   println("iter = ",iter," obj = ",obj)
-      # end
-      if  abs(old - obj) < tol * (old + one(T)) # norm(grad) < tol
-          break
-      else
-          old = obj
-      end  
-  end
-  verbose && print(iters,"  ",obj)
-  return b, iters, obj
+    (obj, old, iters) = (zero(T), zero(T), 0)
+    p = copy(b) # for projection
+    (pvec, idx) = get_model_coefficients(p, intercept)
+    m, n = size(A)
+    
+    # unpack
+    U, s, V = extras.U, extras.s, extras.V
+    z = extras.z
+    b_old = extras.b_old
+    
+    # initialize
+    Ab = A * b
+    
+    # initialize projection
+    project_sparsity_set!(pvec, idx, k)
+    
+    # initialize objective
+    old = eval_objective(Ab, y, b, p, rho, k, intercept)
+    
+    # initialize worker array for Nesterov acceleration
+    copyto!(b_old, b)
+    
+    for iter = 1:ninner
+        iters = iters + 1
+        
+        # Update estimates
+        compute_z!(z, Ab, y)
+        update_beta!(b, U, s, V, z, p, rho / (n-k+1))
+        
+        # update objective
+        copyto!(p, b)
+        project_sparsity_set!(pvec, idx, k)
+        mul!(Ab, A, b)
+        obj = eval_objective(Ab, y, b, p, rho, k, intercept)
+        
+        # check convergence
+        has_converged = abs(old - obj) < tol * (old + one(T)) # norm(grad) < tol
+        
+        if  has_converged
+            break
+        else
+            old = obj
+            
+            # Nesterov acceleration
+            γ = (iter - 1) / (iter + 2 - 1)
+            @inbounds for i in eachindex(b)
+                xi = b[i]
+                yi = b_old[i]
+                zi = xi + γ * (xi - yi)
+                b_old[i] = xi
+                b[i] = zi
+            end
+        end  
+    end
+    verbose && print(iters,"  ",obj)
+    return b, iters, obj
 end
 
 function compute_z!(z, Ab, y)
